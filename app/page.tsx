349dc5583ae5891ff6583c8abdc4311845e87ee4
kekeli <kekelidompeh@gmail.com>
2025-09-17 12:45:48 +0000

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
-import Image from "next/image";
-import { TestTube2, Brain } from "lucide-react";
+import { TestTube2, Brain, Globe } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="font-mono grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] text-4xl row-start-2 items-center sm:items-start">
-        luce industries
+    <div className="font-mono min-h-screen grid grid-rows-[auto_1fr_auto] items-start justify-items-stretch p-4 sm:p-8 lg:p-12 gap-8 sm:gap-12">
+      <main className="row-start-2 w-full max-w-6xl mx-auto pt-24 sm:pt-40 lg:pt-52 flex flex-col items-start justify-center gap-6 sm:gap-8">
+        <div className="w-full flex flex-col gap-4 sm:gap-1">
+          <div className="flex flex-col">
+            <p className="text-4xl sm:text-6xl lg:text-8xl leading-none">luce</p>
+            <p className="text-4xl sm:text-6xl lg:text-8xl leading-none -mt-1 sm:-mt-2 lg:-mt-3">industries</p>
+          </div>
+          <p className="text-base sm:text-lg">
+            build transformative technologies that empower businesses to grow and give hope to everyone whose future depends on bold ideas
+          </p>
+        </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="row-start-3 w-full max-w-7xl mx-auto flex flex-wrap items-center justify-center gap-4 sm:gap-6">
         <a
           className="flex items-center gap-1 hover:underline hover:underline-offset-4"
           href="https://luceindustries.com/lucelabs"
@@ -32,13 +39,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
+          <Globe />
           Go to lucehr.com→
         </a>
       </footer>
