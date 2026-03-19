253b0750ff7a22c8a88e20acb06f6a29e89347e1
kekeli <kekelidompeh@gmail.com>
2026-03-19 06:16:23 +0000

    feat: set favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,13 @@ const spaceMono = Space_Mono({
 export const metadata: Metadata = {
   title: "lucelabs",
   description: "build transformative technologies that empower businesses and give hope to everyone whose future depends on bold ideas",
+  icons: {
+    icon: [
+      { url: "/lucelabslogonew1.svg", type: "image/svg+xml" },
+    ],
+    shortcut: ["/lucelabslogonew1.svg"],
+    apple: ["/lucelabslogonew1.svg"],
+  },
 };
 
 export default function RootLayout({
